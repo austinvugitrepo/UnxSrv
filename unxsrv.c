168506819f4a8aa16aa7aeb876c2cebc7da2f311
Austin Vu <austin@root-user.org>
2026-08-03 03:52:42 +0000

    seems to work, u need root to bind correctly which is fair but a TODO is to connect without using doas or becoming root

--- unxsrv.c
+++ unxsrv.c
@@ -3,24 +3,39 @@
 #include <sys/un.h>
 
 #include <err.h>
-
+#include <string.h>
+#include <unistd.h>
 
 int
 main(void)
 {
 	int sfd, cfd;
 	struct sockaddr_un uaddr;
-	
+	const char *msg = "Hello Unix Socket!\n";
+	const char *filep = "/var/run/unxsrv.sock";
+
 	if ((sfd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
 		err(1, "socket call failed.");
 
 	(void)memset(&uaddr, 0, sizeof(uaddr));
 	uaddr.sun_family = AF_UNIX;
-	uaddr.sun_path = "/var/run/unxsrv.sock"
+	strlcpy(uaddr.sun_path, filep, sizeof(uaddr.sun_path));
 	if (bind(sfd, (struct sockaddr *)&uaddr, sizeof(uaddr)) == -1)
 		err(1, "bind call failed.");
+
 	if (listen(sfd, 1) == -1)
 		err(1, "listen call failed.");
+
+	if ((cfd = accept(sfd, NULL, NULL)) == -1)
+		err(1, "accept call failed.");
+
+ 	if (write(cfd, msg, strlen(msg)) == -1)
+		err(1, "write call failed.");
+
+	(void)close(cfd);
+	(void)close(sfd);
+
+ 	unlink(filep);
 
 	return 0;
 
